845042a023f91f275734e4441368f08522e976b7
Muhammad Hasni <muhammadhsn.hsn@gmail.com>
2020-07-01 02:46:05 +0800

    add check order status bulk

--- order.go
+++ order.go
@@ -28,3 +28,23 @@ func SubmitBulkOrder(data models.BulkOrder) (models.BulkOrderResponse, models.Er
 
 	return k, err1
 }
+
+func GetOrderStatus(data models.BulkOrderStatus) (models.BulkOrderStatusResponse, models.Error) {
+	URL += "/?ac=EPOrderStatusBulk"
+	requestBody, _ := json.Marshal(data)
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("POST", URL, bytes.NewBuffer(requestBody))
+	req.Header.Set("Content-type", "application/json")
+
+	resp, _ := client.Do(req)
+	body, err := ioutil.ReadAll(resp.Body)
+
+	obj, err1 := response(body, err, "order")
+	objString, _ := json.Marshal(obj)
+	k := models.BulkOrderStatusResponse{}
+	json.Unmarshal(objString, &k)
+
+	return k, err1
+}
